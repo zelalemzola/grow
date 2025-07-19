9c03d5d5992bd0910fc1ddc3191a68fc5ffb9e39
zelalemzola <zelezztr@gmail.com>
2025-07-20 01:47:41 +0300

    done

--- src/app/sign-in/[[...sign-in]]/page.tsx
+++ src/app/sign-in/[[...sign-in]]/page.tsx
@@ -3,7 +3,7 @@ import { SignIn } from '@clerk/nextjs'
 export default function Page() {
   return (
 
-    <div className="flex items-center justify-center">
+    <div className="h-full w-full flex items-center justify-center">
      <SignIn />
     
     </div>
